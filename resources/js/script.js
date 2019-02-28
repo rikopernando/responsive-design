9d95c5f33f471dda7d68d17f0b4c8acf730f1701
rikopernando <rikopernando96@gmail.com>
2019-02-28 16:57:43 +0700

    adding animation on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -57,4 +57,28 @@ $(document).ready(function() {
         }
       });
 
+      $('.js--wp-1').waypoint(function(direction) {
+          $('.js--wp-1').addClass('animated fadeIn');
+      },{
+        offset: '50%'
+      });
+
+      $('.js--wp-2').waypoint(function(direction) {
+          $('.js--wp-2').addClass('animated fadeInUp');
+      },{
+        offset: '50%'
+      });
+
+      $('.js--wp-3').waypoint(function(direction) {
+          $('.js--wp-3').addClass('animated fadeIn');
+      },{
+        offset: '50%'
+      });
+
+      $('.js--wp-4').waypoint(function(direction) {
+          $('.js--wp-4').addClass('animated pulse');
+      },{
+        offset: '50%'
+      });
+
 });
